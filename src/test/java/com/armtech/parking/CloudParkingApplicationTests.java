97e2ec24e912152c6a9d0dbada2cd019fcc906a8
Adilson-R-Martins <adilson.martins@protonmail.com>
2025-06-24 14:28:04 -0300

    Estrutura Inicial

---
+++ src/test/java/com/armtech/parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armtech.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
